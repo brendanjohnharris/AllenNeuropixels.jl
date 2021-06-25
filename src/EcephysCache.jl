68c795fc001be15059d7394485f49181ae7844ab
brendanjohnharris <brendanjohnharris@gmail.com>
2021-06-26 01:13:09 +1000

    Load directly from saved data
    Former-commit-id: 538d0951f44cd68f256ade9f64c8100876705a0f

--- src/EcephysCache.jl
+++ src/EcephysCache.jl
@@ -1,29 +1,38 @@
 function cache()
-    ecephys_project_cache.EcephysProjectCache.from_warehouse(manifest=manifestdir)
+    ecephys_project_cache.EcephysProjectCache.from_warehouse(manifest=manifestpath)
 end
 
 # These will take a while to download
-function get_session_table()
-    types = [String, Int64, String, Float64, Symbol, String, Int, Int, Int, Vector]
-    df = convertdataframe(cache().get_session_table(), types)
-    return df
+function get_session_table(file="sessions.csv", dir=datadir) # Really, this should be called with no arguments
+    if !isfile(abspath(dir, file))
+        cache().get_sessions() # Download data
+    end
+    loaddataframe(file, dir)
 end
 export get_session_table
 
-function get_probes()
-    types = [Int64, Float64, String, String, Float64, Bool, Int64, Int64, Vector]
-    convertdataframe(cache().get_probes(), types)
+function get_probes(file="probes.csv", dir=datadir)
+    if !isfile(abspath(dir, file))
+        cache().get_probes()
+    end
+    loaddataframe(file, dir)
 end
 export get_probes
 
-function get_channels()
-    types = [Int, Int, Int, Int, Int, Int, Int, Int, Symbol, Int, Float64, Symbol, Float64, Bool, Int64]
-    convertdataframe(cache().get_channels(), types)
+function get_channels(file="channels.csv", dir=datadir)
+    if !isfile(abspath(dir, file))
+        cache().get_channels()
+    end
+    loaddataframe(file, dir)
 end
 export get_channels
 
-# function get_units()
-#     types = [Int, Int, Int, Int, Int, Int, Int, Int, Symbol, Int, Float64, Symbol, Float64, Bool, Int64]
-#     convertdataframe(cache().get_units(), types)
-# end
-# export get_units
+
+function get_units(file="units.csv", dir=datadir)
+    if !isfile(abspath(dir, file))
+        cache().get_units()
+    end
+    loaddataframe(file, dir)
+end
+export get_units
+
