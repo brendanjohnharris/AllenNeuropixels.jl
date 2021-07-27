5fa39735a8d5fb1e48e537f668972154e3378c41
brendanjohnharris <brendanjohnharris@gmail.com>
2021-07-27 10:44:32 +1000

    Get frames of natural stimuli
    Former-commit-id: 947239c768cde5f9355aecd6f3929671791fbb91

--- src/BrainObservatory.jl
+++ src/BrainObservatory.jl
@@ -11,3 +11,28 @@ function getspatialgrating(; height=100, aspect_ratio=2, ori=45, pix_per_cycle=1
     stimulus_info.get_spatial_grating(; height, aspect_ratio, ori, pix_per_cycle, phase, p2p_amp, baseline)
 end
 
+
+function getnaturalmovie(stimulus="natural_movie_one")
+    boc = getophysexperiments(stimuli=[stimulus])[1] # Just need one of these sessions to get the template
+    ds = brainobservatorycache().get_ophys_experiment_data(boc["id"])
+    template = ds.get_stimulus_template(stimulus)
+end
+
+function getnaturalmovieframes(session, times)
+    df = getstimuli(session, times)
+    movies = unique(df.stimulus_name)
+    @assert all(.∈(movies , (["natural_movie_one", "natural_movie_three", "natural_scenes"],)))
+    movieframes = [getnaturalmovie(movie) for movie in movies]
+    res = size(movieframes[1])[2:3]
+    frames = Array{typeof(movieframes[1][1])}(undef, length(times), res...)
+    for t in 1:length(times)
+        movieidx = indexin([df.stimulus_name[t]], movies)[1]
+        frame = Int(Meta.parse(df.frame[t])+1) # Goddamn python frames start at 0
+        if frame < 1
+            frames[t, :, :] = fill(NaN, (1, size(frames)[2:3]...))
+        else
+            frames[t, :, :] = movieframes[movieidx][frame, :, :]
+        end
+    end
+    return DimArray(frames, (Dim{:window}(times), DimensionalData.X(1:size(frames, 2)), DimensionalData.Y(1:size(frames, 3))))
+end
