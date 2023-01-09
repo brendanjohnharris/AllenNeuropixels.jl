62afd5e5e41c4391b5304350d864ee6b927c0737
brendanjohnharris <brendanjohnharris@gmail.com>
2023-01-09 22:53:35 +1100

    Rm JSServe
    Former-commit-id: 1e281417828e0ec04bd00ecfc08369a7a1f9b007

--- src/Plots/Plots.jl
+++ src/Plots/Plots.jl
@@ -1,6 +1,6 @@
 module Plots
     using ..Makie
-    using JSServe
+    # using JSServe
     using Colors
     using Meshing
     using MeshIO
@@ -10,23 +10,23 @@ module Plots
     using FileIO
     import ..AllenNeuropixels as AN
 
-    function htmlexport(fig, file::String="plot.html")
-        open(file, "w") do io
-                println(io, """
-                <html>
-                    <head>
-                    </head>
-                    <body>
-                """)
-                show(io, MIME"text/html"(), Page(exportable=true, offline=true))
-                show(io, MIME"text/html"(), fig)
-                println(io, """
-                    </body>
-                </html>
-                """)
-        end
-    end
-    exporthtml = htmlexport
+    # function htmlexport(fig, file::String="plot.html")
+    #     open(file, "w") do io
+    #             println(io, """
+    #             <html>
+    #                 <head>
+    #                 </head>
+    #                 <body>
+    #             """)
+    #             show(io, MIME"text/html"(), Page(exportable=true, offline=true))
+    #             show(io, MIME"text/html"(), fig)
+    #             println(io, """
+    #                 </body>
+    #             </html>
+    #             """)
+    #     end
+    # end
+    # exporthtml = htmlexport
 
     Makie.convert_arguments(x::AN.LFPVector) = (dims(x, Ti)|>collect, x|>Array)
     Makie.convert_single_argument(x::AN.LFPVector) = (dims(x, Ti)|>collect, x|>Array)
