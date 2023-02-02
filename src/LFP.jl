bb649980e60390b13302a48f1ef13b68e9420e44
brendanjohnharris <brendanjohnharris@gmail.com>
2023-02-02 18:29:34 +1100

    Ups

--- src/LFP.jl
+++ src/LFP.jl
@@ -226,17 +226,17 @@ end
 """
 Now we can overload `getlfp()` to index by structure
 """
-function getlfp(session::AbstractSession, probeid::Int, structures::Union{Vector{String}, String}; kwargs...)
+function getlfp(session::AbstractSession, probeid::Int, structures::Union{Vector{<:AbstractString}, AbstractString}; kwargs...)
     if structures isa String
         structures = [structures]
     end
-    channels = subset(getchannels(session, probeid), :ecephys_structure_acronym=>ByRow(∈(structures)), skipmissing=true)
+    channels = subset(getchannels(session, probeid), :ecephys_structure_acronym=>ByRow(==(structures)), skipmissing=true)
     channels = channels.id ∩ getlfpchannels(session, probeid)
     isempty(channels) && @error "No matching channels found for structure(s) $structures. Perhaps you have entered the wrong probe id?"
     getlfp(session, probeid; channels, kwargs...)
 end
 
-function getlfp(S::AbstractSession, structure::String; kwargs...)
+function getlfp(S::AbstractSession, structure::AbstractString; kwargs...)
     probeid = structure2probe(S, structure)
     getlfp(S, probeid, structure; kwargs...)
 end
