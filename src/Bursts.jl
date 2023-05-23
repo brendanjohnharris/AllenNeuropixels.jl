04fed2d6736bd93552d67e0b69ae61ce56566045
brendanjohnharris <brendanjohnharris@gmail.com>
2023-05-23 12:10:28 +1000

    Update LFP-LFP

--- src/Bursts.jl
+++ src/Bursts.jl
@@ -784,7 +784,7 @@ function _phaselockingindex(x::LFPVector, y::LFPVector; pass=nothing)
 end
 
 function phaselockingindex(x::LFPVector, y::LFPVector; pass=nothing)
-    phis = _phaselockingindex(x, y; pass)
+    phis = collect(_phaselockingindex(x, y; pass))
     γ = pairwisephaseconsistency(phis)
     𝑝 = isempty(phis) ? 1.0 : HypothesisTests.pvalue(RayleighTest(phis))
     return γ, 𝑝
