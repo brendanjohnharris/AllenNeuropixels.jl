b5cafeaffa98ea6ba99dbd6cefd016b7d47283af
brendanjohnharris <bhar9988@uni.sydney.edu.au>
2024-01-17 13:35:57 +1100

    Stimulus partition for VBN flashes
    Former-commit-id: e3b05d4e0279600d2c705f2fd4f0dc0223a568d7 [formerly 1910b92fdb3c5a6d488d4d9518a3dfa0eb95ba42]
    Former-commit-id: c3cec1f541359cf50ea0f9b15de2494dcbb25021

--- src/AllenNeuropixels.jl
+++ src/AllenNeuropixels.jl
@@ -11,11 +11,11 @@ using IntervalSets
 using Reexport
 using PythonCall
 @reexport using AllenNeuropixelsBase
+import AllenNeuropixelsBase.rectifytime
 function __init__()
     @require Makie="ee78f7c6-11fb-53f2-987a-cfe4a2b5a57a" @eval include("./Plots/Plots.jl")
     # @require GLMakie="e9467ef8-e4e7-5192-8a1a-b1aee30e663a" @eval using .Plots
 end
-
 
 include("./Windows.jl")
 include("./LFP.jl")
